24be50a54b64ab0334e95274aef4b2be36f71bb7
Hongda Chen <58559424+HongdaChen@users.noreply.github.com>
2022-06-02 14:03:43 +0800

    Add files via upload

--- papermove.py
+++ papermove.py
@@ -1,4 +1,21 @@
 import os, shutil, time
+import logging
+############################################## log ##########################################################
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+# handler = logging.FileHandler("log.txt")
+# handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#handler.setFormatter(formatter)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+
+# logger.addHandler(handler)
+logger.addHandler(console)
+
+############################################## do ##########################################################
+
 root_dir = "F:/cache/"
 target_dir = "F:/"
 file_list = os.listdir(root_dir)
@@ -22,9 +39,16 @@ for i in file_list:
         except:
             print("{0} failed to be created!".format(today_folder))
         # move .pdf from cach to coresponding date folder
-        shutil.move(file_path,today_folder)
+        try:
+            shutil.move(file_path,today_folder)
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except Exception:
+            logger.error("Faild to move", exc_info=True)
+
 if flag == 0:
     print("there are no files in the folder:{0}".format(root_dir))
+    
 print("work done!")
 print("moved {0} files".format(today_total))
 input("press <Enter> to quit")
